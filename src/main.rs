addad4dcb2503c65116525b81d8b53d739de6935
iGiant <sergey.grishayev@gmail.com>
2023-11-12 22:35:26 +0400

    first try

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,44 @@ const SIZE: usize = 8;
 const LETTERS: [char; SIZE] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 struct Board([[u8; SIZE]; SIZE]);
 
+impl Board {
+    fn new() -> Self {
+        Self([[0; SIZE]; SIZE])
+    }
+    fn get(&self, col: usize, row: usize) -> Option<u8> {
+        if col >= SIZE || row >= SIZE {
+            println!("{}, {}", col, row);
+            return None;
+        }
+        Some(self.0[row][col])
+    }
+    fn set(&mut self, col: usize, row: usize, value: u8) -> Result<(), String> {
+        if col > (SIZE - 1) || row > (SIZE - 1) {
+            return Err(String::from("Индекс вне диапазона"));
+        }
+        self.0[row][col] = value;
+        Ok(())
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "  ")?;
+        for letter in LETTERS {
+            write!(f, "{:>3}", letter)?;
+        }
+        writeln!(f, "\n    {}", "-".repeat(3 * SIZE - 2))?;
+        for i in 1..=self.0.len() {
+            write!(f, "{}|", SIZE - i + 1)?;
+            for cell in self.0[SIZE - i] {
+                write!(f, "{:>3}", cell)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
 struct BoardNotation {
     letter: char,
     digit: usize,
@@ -56,53 +94,64 @@ impl fmt::Display for BoardNotation {
         write!(f, "{}{}", self.letter, self.digit)
     }
 }
-
-impl Board {
-    fn new() -> Self {
-        Self([[0; SIZE]; SIZE])
-    }
-    fn get(&self, col: usize, row: usize) -> Option<u8> {
-        if col > (SIZE - 1) || row > (SIZE - 1) {
-            return None;
-        }
-        Some(self.0[row][col])
-    }
-    fn set(&mut self, col: usize, row: usize, value: u8) -> Result<(), String> {
-        if col > (SIZE - 1) || row > (SIZE - 1) {
-            return Err(String::from("Индекс вне диапазона"));
-        }
-        self.0[row][col] = value;
-        Ok(())
-    }
+#[derive(PartialEq, Eq)]
+enum Solved {
+    Yes,
+    No,
 }
 
-impl fmt::Display for Board {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "  ")?;
-        for letter in LETTERS {
-            write!(f, "{:>3}", letter)?;
-        }
-        writeln!(f, "\n    {}", "-".repeat(3 * SIZE - 2))?;
-        for i in 1..=self.0.len() {
-            write!(f, "{}|", SIZE - i)?;
-            for cell in self.0[SIZE - i] {
-                write!(f, "{:>3}", cell)?;
-            }
-            writeln!(f)?;
-        }
-        Ok(())
+fn next(col: usize, row: usize, board: &Board) -> Option<(usize, usize)> {
+    if col >= 2 && row >= 1 && board.get(col - 2, row - 1).unwrap() == 0 {
+        return Some((col - 2, row - 1));
     }
+    if col >= 1 && row >= 2 && board.get(col - 1, row - 2).unwrap() == 0 {
+        return Some((col - 1, row - 2));
+    }
+    if col >= 2 && row < SIZE - 1 && board.get(col - 2, row + 1).unwrap() == 0 {
+        return Some((col - 2, row + 1));
+    }
+    if col >= 1 && row < SIZE - 2 && board.get(col - 1, row + 2).unwrap() == 0 {
+        return Some((col - 1, row + 2));
+    }
+    if col < SIZE - 1 && row >= 2 && board.get(col + 1, row - 2).unwrap() == 0 {
+        return Some((col + 1, row - 2));
+    }
+    if col < SIZE - 2 && row >= 1 && board.get(col + 2, row - 1).unwrap() == 0 {
+        return Some((col + 2, row - 1));
+    }
+    if col < SIZE - 2 && row < SIZE - 1 && board.get(col + 2, row + 1).unwrap() == 0 {
+        return Some((col + 2, row + 1));
+    }
+    if col < SIZE - 1 && row < SIZE - 2 && board.get(col + 1, row + 2).unwrap() == 0 {
+        return Some((col + 1, row + 2));
+    }
+    None
 }
 
-// fn next(col: usize, row: usize, field: &Board) -> Option<(usize, usize)> {}
+fn solve(col: usize, row: usize, count: u8, board: &mut Board) -> Solved {
+    if count > (SIZE * SIZE) as u8 {
+        return Solved::Yes;
+    }
+    let _ = board.set(col, row, count);
+    let (col, row) = match next(col, row, board) {
+        Some((c, r)) => (c, r),
+        None => {
+            let _ = board.set(col, row, 0);
+            return Solved::No;
+        }
+    };
+    solve(col, row, count + 1, board)
+}
 
 fn main() -> Result<(), String> {
     let mut board = Board::new();
     board.set(0, 7, 1)?;
     println!("{}", board);
-    board.set(3, 3, 2)?;
-    println!("{}", board);
-    println!("\n{}", board.get(3, 3).unwrap());
-    println!("{}", BoardNotation::from((3, 0)));
+    if solve(0, 7, 1, &mut board) == Solved::Yes {
+        println!("SOLVED!!!\n{}", board)
+    } else {
+        println!("NOT SOLVED!!!\n{}", board)
+    }
+
     Ok(())
 }
